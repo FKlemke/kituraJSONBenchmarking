66c1d818021ba4f9de1f4b552af7da5d02727230
Felix E. C. Klemke <me@felixklemke.com>
2017-08-29 19:25:55 +0200

    Large HTML pic for page adde to check behavior on high memory load

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "kituraJSONBenchmarking",
+    name: "kituraBenchmarking",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", versions: Version(0,0,0)..<Version(50,0,0)),
